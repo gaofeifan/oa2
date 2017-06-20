f9d065857771bb00352f62a7b1ec59d0d3dffa2a
m.r.li <m.r.li@medilink.com.cn>
2017-06-20 17:41:38 +0800

    申请表单基础类

---
+++ src/main/java/com/pj/flow/pojo/FlowEntry.java
@@ -0,0 +1,172 @@
+package com.pj.flow.pojo;
+
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="flow_entry")
+public class FlowEntry {
+	@Id
+	@GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    private String number;
+
+    private Integer recruitId;
+
+    private String name;
+
+    private Integer sex;
+
+    private String phone;
+
+    private String email;
+
+    private Date entryDate;
+
+    private String probation;
+
+    private String serviceYears;
+
+    private String remark;
+
+    private String checkUrl;
+
+    private String resumeUrl;
+
+    private String registerUrl;
+
+    private String state;
+
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number == null ? null : number.trim();
+    }
+
+    public Integer getRecruitId() {
+        return recruitId;
+    }
+
+    public void setRecruitId(Integer recruitId) {
+        this.recruitId = recruitId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Date getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(Date entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public String getProbation() {
+        return probation;
+    }
+
+    public void setProbation(String probation) {
+        this.probation = probation == null ? null : probation.trim();
+    }
+
+    public String getServiceYears() {
+        return serviceYears;
+    }
+
+    public void setServiceYears(String serviceYears) {
+        this.serviceYears = serviceYears == null ? null : serviceYears.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getCheckUrl() {
+        return checkUrl;
+    }
+
+    public void setCheckUrl(String checkUrl) {
+        this.checkUrl = checkUrl == null ? null : checkUrl.trim();
+    }
+
+    public String getResumeUrl() {
+        return resumeUrl;
+    }
+
+    public void setResumeUrl(String resumeUrl) {
+        this.resumeUrl = resumeUrl == null ? null : resumeUrl.trim();
+    }
+
+    public String getRegisterUrl() {
+        return registerUrl;
+    }
+
+    public void setRegisterUrl(String registerUrl) {
+        this.registerUrl = registerUrl == null ? null : registerUrl.trim();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
